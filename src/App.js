07d1b0aa38d52a902434bda10c2ceeae402bd41f
Austin Carman <abcarman12@gmail.com>
2021-05-24 11:54:17 -0600

    created components-folder, started App.js building

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Title goes here</h1>
-    </div>
+    <Router> 
+      <div className="App">
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/sign-up'>Sign-Up</Link>
+        <Link to='/profile'>Profile</Link>
+
+        <Switch>
+          <Route path='/login'>
+            {/* <Login /> */}
+          </Route>
+          
+          <Route path='/sign-up'>
+            {/* <SignUP /> */}
+          </Route>
+
+          <Route path='/profile'>
+            {/* <Profile /> */}
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
