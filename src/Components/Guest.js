ffaffe9322d1c7d724154368a44d6f7392124d31
Austin Carman <abcarman12@gmail.com>
2021-05-27 14:45:25 -0600

    guest should be able to rsvp now

--- src/Components/Guest.js
+++ src/Components/Guest.js
@@ -1,78 +1,68 @@
-// this is for RSVP and picking a food to bring
-//not functional yet
-
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
-const initialResponse = {
-    rsvp: '',
-    guest_id: '', // will be coming from reducer instead of local state
-}
-
-const initialAssignments = {
-    item_name: '',
-    responsible_for: '',
-}
 
 const Guest = (props) => {
-    const { user_id, potluck } = props // this needs to be passed in from somewhere, user_id is from Login, potluck is same as state in guestPotluckCard
+    const { potluck, id } = props;
 
-    const [ response, setResponse ] = useState(initialResponse);
-    const [ assignments, setAssignments ] = useState(initialAssignments); // still need to make assignments functionality
+    const [ response, setResponse ] = useState({
+        rsvp: '',
+        guest_id: id
+    });
 
-
-    //pass in potluck where event_id matches e.target -> this potluck will have items in data
-
-    useEffect(() => { // this is for guest response
-        axios.get(`https://ft-potluck-planner-5.herokuapp.com/api/events/${props.guestPotluck.event_id}/guests`, response)
-            .then(res => {
-                console.log('guest', res);
-                //setResponse
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, [])
+    const { push } = useHistory();
 
     const onChange = (e) => {
         const { name, value } = e.target;
-        // set response to update rsvp with name/value
-        // use guest id from reducer to 
+        setResponse({
+            ...response,
+            [name]: value
+        })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        //push back to profile
+        axiosWithAuth()
+            .put(`https://ft-potluck-planner-5.herokuapp.com/api/events/${potluck.event_id}/guests`, response)
+            .then(res => {
+                push('/profile')
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     return(
         <div>
             <div className='guest-potluck-card'>
-            <p>{props.guestPotluck.event_date}</p>
-            <p>{props.guestPotluck.event_time}</p>
-            <p>{props.guestPotluck.event_location}</p>
-            <p>{props.guestPotluck.organizer}</p>
+            <p>{potluck.event_date}</p>
+            <p>{potluck.event_time}</p>
+            <p>{potluck.event_location}</p>
+            <p>{potluck.organizer}</p>
             <form onSubmit={handleSubmit}>
                 <p>Will you be attending the potluck?</p>
                 <label> Yes
                     <input 
                         type='radio' 
-                        name='response' 
+                        name='rsvp' 
                         value='yes'
                         checked={response.rsvp === 'yes'}
                         onChange={onChange}
                         />
                 </label>
+
                 <label> No
                     <input 
                         type='radio' 
-                        name='response' 
+                        name='rsvp' 
                         value='no'
                         checked={response.rsvp === 'no'}
                         onChange={onChange}
                         />
                 </label>
+                <button>Save RSVP</button>
             </form>
         </div>
 
@@ -82,7 +72,8 @@ const Guest = (props) => {
 
 const mapToStateProps = (state) => {
     return({
-        guestPotluck: state.guestPotluck
+        potluck: state.guestPotluck,
+        id: state.guest_id
     })
 }
 
